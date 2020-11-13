97fee532d3aba8f50a92508260888aacd2d4c4a9
Sanjay Mahajan <49758153+sanjaygeeky@users.noreply.github.com>
2020-11-13 20:31:51 +0530

    Update devops-demo.sql

--- devops-demo.sql
+++ devops-demo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE dbversion (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,version VARCHAR(30) NOT NULL);
 INSERT INTO dbversion (version) VALUES ('1.0');
-CREATE USER 'devops'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'devops'@'%' IDENTIFIED BY 'Chintoo009@';
 GRANT ALL PRIVILEGES ON 'devopsdb' TO 'devops'@'%';
 flush PRIVILEGES;
