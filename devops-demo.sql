212883e47c54b7d019173c6ee1bd90ba0cb7ad36
Sanjay Mahajan <49758153+sanjaygeeky@users.noreply.github.com>
2020-11-26 18:09:56 +0530

    Updating the sql for the right format

--- devops-demo.sql
+++ devops-demo.sql
@@ -1,7 +1,8 @@
+create database devopsdb;
 USE devopsdb;
+drop user 'newuser'@'localhost';
 CREATE TABLE dbversion (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,version VARCHAR(30) NOT NULL);
-INSERT INTO dbversion (version) VALUES ('1.0');
+INSERT INTO dbversion (version) VALUES ('300.0');
 CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';
-GRANT ALL PRIVILEGES ON 'devopsdb' TO 'newuser'@'localhost' WITH GRANT OPTION ;
-flush PRIVILEGES;
+GRANT ALL PRIVILEGES ON devopsdb.* TO 'newuser'@'localhost';
+FLUSH PRIVILEGES;
